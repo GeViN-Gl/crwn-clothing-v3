ce6032aba4d903ba991e9e4a9f6c3be11b2af14c
Oleg Koval <gevin.gl@gmail.com>
2023-02-23 20:38:17 +0200

    create directory with elements inside

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,16 @@
+import './directory.styles.scss';
+import CategoryItem from '../category-item/category-item.component';
+
+import React from 'react';
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
